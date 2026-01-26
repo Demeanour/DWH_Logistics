2fc16eb107fc3212ce9c6e29805d81c998cb6333
Kenneth Motsoari <tken.motsoari@gmail.com>
2026-01-26 19:50:01 +0200

    Update [01] Create Database.sql

--- scripts/[01] Create Database.sql
+++ scripts/[01] Create Database.sql
@@ -1,3 +1,17 @@
+/*
+=====================================================
+Create database and Schemas
+=====================================================
+Script Purpose:
+	This script creates a new database name 'DataWarehouse' after checking if it already exists.
+	If the database exists, it is dropped and recreated. Additionally, the script sets up three schemas
+	with in the database: Bronze, Silver, and Gold
+
+Warning:
+	Running this script will drop the entire database if it exists.
+	All the data in the database will be permanently deleted. Proceed with caution
+	and ensure that you have proper backups before running this script
+*/
 -- drop and recreate the 'DataWarehouse' database.
 if EXISTS (SELECT 1 from sys.databases where name = 'DataWarehouse')
 BEGIN
