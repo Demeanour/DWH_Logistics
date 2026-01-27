b33fc3585755316b4b4de6044c7c40faeb471f11
Kenneth Motsoari <tken.motsoari@gmail.com>
2026-01-27 16:51:31 +0200

    Update bronze_layer_script.sql
    updated bronze_layer Script for creating tables

--- scripts/bronze_layer_script.sql
+++ scripts/bronze_layer_script.sql
@@ -1,4 +1,6 @@
 USE DataWarehouse;
+
+-- Create Customers table
 IF OBJECT('bronze_customers', 'U') IS NOT NULL
 	DROP TABLE bronze_customers
 create table bronze_customers(
@@ -11,3 +13,22 @@ account_status varchar(50),
 contract_start_date DATE ,
 annual_revenue_potential int
 );
+
+-- Create Deliver_event table
+IF OBJECT('bronze_delivery_events', 'U') IS NOT NULL
+	DROP TABLE bronze_delivery_events
+create table bronze_delivery_events(
+event_id varchar(50),
+load_id varchar(50),
+trip_id varchar(50),
+event_type varchar(50),
+facility_id varchar(50),
+scheduled_datetime dateTime,
+actual_datetime datetime,
+detention_minutes varchar(50),
+on_time_flag varchar(50),
+location_city varchar(50),
+location_state varchar(50)
+);
+
+
